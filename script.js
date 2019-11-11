565ac22d77faaf5410d9ea8154ee9d063a8636c5
fredwilbreadmill <57623332+fredwilbreadmill@users.noreply.github.com>
2019-11-11 22:28:35 +1100

    Initial Submission
    Finalised code for the A4

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('SignIn').addEventListener('click', function () {
+    document.getElementById('bg-modal').style.display = 'flex';
+});
+
+document.getElementById('close').addEventListener('click', function () {
+    document.getElementById('bg-modal').style.display = 'none';
+})
+
